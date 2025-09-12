e8b72c13b53c6b95bc8e6f11c698d93b5323ba2a
Abigail <abigailvaniapra@gmail.com>
2025-09-13 02:24:42 +0700

    Commit No. 3 - use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 
 interface CoffeeListener {
     fun onSelected(id: Int)
@@ -15,46 +17,65 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if(savedInstanceState == null){
+            findViewById<FragmentContainerView>(R.id.fragment_container).let {
+                containerLayout ->
+                    val listFragment = ListFragment()
+                    supportFragmentManager.beginTransaction()
+                        .add(containerLayout.id, listFragment)
+                        .commit()
+            }
+        }
     }
 
     override fun onSelected (id: Int){
-        val detailFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_detail)
-            as DetailFragment
-        detailFragment.setCoffeeData(id)
+//        val detailFragment = supportFragmentManager
+//            .findFragmentById(R.id.fragment_detail)
+//            as DetailFragment
+//        detailFragment.setCoffeeData(id)
+        findViewById<FragmentContainerView>(R.id.fragment_container).let {
+            containerLayout ->
+                val detailFragment = DetailFragment.newInstance(id)
+                supportFragmentManager.beginTransaction()
+                    .replace(containerLayout.id, detailFragment)
+                    .addToBackStack(null)
+                    .commit()
+
+        }
     }
 
-    override fun onStart(){
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume(){
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause(){
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop(){
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroy(){
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+//    override fun onStart(){
+//        super.onStart()
+//        Log.d(TAG, "onStart")
+//    }
+//
+//    override fun onResume(){
+//        super.onResume()
+//        Log.d(TAG, "onResume")
+//    }
+//
+//    override fun onPause(){
+//        super.onPause()
+//        Log.d(TAG, "onPause")
+//    }
+//
+//    override fun onStop(){
+//        super.onStop()
+//        Log.d(TAG, "onStop")
+//    }
+//
+//    override fun onDestroy(){
+//        super.onDestroy()
+//        Log.d(TAG, "onDestroy")
+//    }
+//
+//    companion object {
+//        private const val TAG = "MainActivity"
+//    }
 }
